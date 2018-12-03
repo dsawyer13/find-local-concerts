ab2f4ac0a8308fbfc42dc868b0e08240fc024665
Dylan Sawyer <dsawyer@tutanota.com>
2018-12-03 18:33:02 -0500

    added comments to javaScript

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict';
-
+//ticketmaster API url and API key
 const tmApiKey = 'p48FaIihgbRfceXm48vMgx9sScj9hrus';
 const tmURL = "https://app.ticketmaster.com/discovery/v2/events.json";
-
+//initialize rapidAPI library
 var rapid = new RapidAPI("find-local-shows_5bf6080de4b08725af2b0d57", "b306b59c-5cb9-4d9f-9df6-e268b26cc3a6");
 
-
+//listens for submit button click event
 function watchInput() {
   $('.startForm').submit(event => {
     event.preventDefault();
@@ -22,7 +22,7 @@ function watchInput() {
     }
   })
 };
-
+//listens for submit button click event on the NavBar
 function watchNavBar() {
   $('.bar').submit(event => {
     event.preventDefault();
@@ -31,7 +31,7 @@ function watchNavBar() {
     $('.bar').reset();
   })
 }
-
+//listens for h1 click event, returns user to Landing page
 function watchHomeButton() {
   $('.navh1').on('click', event => {
     $('.appendNav').css('display', 'none');
@@ -40,7 +40,7 @@ function watchHomeButton() {
   }
 )
 }
-
+//uses fetch to get data from ticketMaster API
 function getConcerts(searchTerm) {
   let params = {
     city: searchTerm,
@@ -71,13 +71,13 @@ function getConcerts(searchTerm) {
 
 
 
-
+//takes query parameters and returns them in a form that can be used by fetch
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 };
-
+//creates li item for each concert and appends them to #results-list
 function displayConcertResults(responseJson) {
   console.log(responseJson);
   $('#js-error-message').empty();
@@ -115,7 +115,8 @@ function displayConcertResults(responseJson) {
 }
 
 
-
+//uses RapidAPI to call iTunes api
+//iTunes API response data appended to each .audioPlayer
 function getAudioPlayer() {
   $('#results-list').on('click', '.getAudioButton', event => {
 
