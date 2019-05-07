17ba4dfa64f7dc26edf81d3799c5afeedf50d534
dsawyer13 <dsawyer@tutanota.com>
2019-05-06 21:02:24 -0400

    split landing page and results page into different files

--- app.js
+++ app.js
@@ -1,150 +1,69 @@
-'use strict';
+"use strict";
 //ticketmaster API url and API key
-const tmApiKey = 'p48FaIihgbRfceXm48vMgx9sScj9hrus';
+const tmApiKey = "p48FaIihgbRfceXm48vMgx9sScj9hrus";
 const tmURL = "https://app.ticketmaster.com/discovery/v2/events.json";
-//initialize rapidAPI library
-var rapid = new RapidAPI("find-local-shows_5bf6080de4b08725af2b0d57", "b306b59c-5cb9-4d9f-9df6-e268b26cc3a6");
 
 //listens for submit button click event
 function watchInput() {
-  $('.startForm').submit(event => {
+  $(".startForm").submit(event => {
     event.preventDefault();
-
-    const searchTerm = $('.search-text').val();
-
-    const startVisible = $('.startPage').css('display');
-
-    if (startVisible !== 'none') {
-      $('.startPage').css('display', 'none');
-      $('.appendNav').css('display', 'block');
+    const searchTerm = $(".search-text").val();
+    if (searchTerm) {
       getConcerts(searchTerm);
-      $('.startForm').text('');
+      window.location.href = "results.html"
     } else {
-      watchNavBar();
+      $(".error-message").text("Please Input City");
     }
-  })
-};
-//listens for submit button click event on the NavBar
+  });
+}
 function watchNavBar() {
   $('.bar').submit(event => {
     event.preventDefault();
-    const searchTerm2 = $('.nav-search-text').val();
-    getConcerts(searchTerm2);
-    $('.bar').text('');
+    const searchTerm = $('.search-text').val();
+    if (searchTerm) {
+      getConcerts(searchTerm);
+      location.reload();
+    } else {
+      $(".error-message").text("Please Input City");
+    }
   })
 }
-//listens for h1 click event, returns user to Landing page
-function watchHomeButton() {
-  $('.navh1').on('click', event => {
-    $('.appendNav').css('display', 'none');
-    $('.startPage').css('display', 'block')
-    $('#results-list').empty();
-  }
-)
-}
-//uses fetch to get data from ticketMaster API
-function getConcerts(searchTerm) {
+
+function getConcerts(city) {
   let params = {
-    city: searchTerm,
+    city: city,
     apikey: tmApiKey,
-    countryCode: 'US',
-    classificationName: 'music'
+    countryCode: "US",
+    classificationName: "music",
+    sort: 'date,asc'
   };
   const queryString = formatQueryParams(params);
-  const url = tmURL + '?' + queryString;
+  const url = tmURL + "?" + queryString;
 
   fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => {displayConcertResults(responseJson);console.log(responseJson);})
-    .catch(err => {
-      alert("Invalid Search. Please try again.");
+    .then(responseJson => {
+      localStorage.setItem("concerts", JSON.stringify(responseJson));
     })
-};
-
-
-
-
+    .catch(err => {
+      console.log(err);
+    });
+}
 
 //takes query parameters and returns them in a form that can be used by fetch
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
-};
-//creates li item for each concert and appends them to #results-list
-function displayConcertResults(responseJson) {
-
-  $('#js-error-message').empty();
-  $('#results-list').empty();
-   for (let i = 0; i < 10; i++) {
-     var date = new Date(responseJson._embedded.events[i].dates.start.localDate);
-     const fixDate = ((date.getMonth()+1)+ '/' + date.getDate() + '/' + date.getFullYear());
-     let artistName = `${responseJson._embedded.events[i]._embedded.attractions[0].name}`;
-     const pictures = responseJson._embedded.events[i].images;
-     const highResPic = pictures.filter(picture => {
-       return picture.ratio === "4_3";
-     })
-     $('#results-list').append(
-       `<li class="eachResult">
-
-            <div class="picContainer">
-              <img class="concertPic" src="${highResPic[0].url}" width="225px" height="160px" class="show-image" alt="${responseJson._embedded.events[i].name}">
-            </div>
-            <div class="info">
-              <div class="textBlock">
-                <div class="date">${fixDate}</div>
-                <div class="title">${responseJson._embedded.events[i].name}</div>
-                <div class="concertLocation">@ ${responseJson._embedded.events[i]._embedded.venues[0].name}</div>
-              </div>
-              <div class="button">
-                <button class="getAudioButton" artistName='${artistName}' item-index="${i}">Play Music</button>
-                <button class="tickets" onClick="window.open('${responseJson._embedded.events[i].url}')">Buy Tickets</button>
-                <div class='audioPlayer'></div>
-              </div>
-            </div>
-
-        </li>
-       `
-
-     )
-
-   };
-  getAudioPlayer();
+  const queryItems = Object.keys(params).map(
+    key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
 }
 
-
-//uses RapidAPI to call iTunes api
-//iTunes API response data appended to each .audioPlayer
-function getAudioPlayer() {
-  $('#results-list').on('click', '.getAudioButton', event => {
-
-    const itemIndex = event.target.getAttribute('item-index');
-    const artist = event.target.getAttribute('artistName');
-
-    rapid.call('iTunes', 'search', {
-      'term': artist,
-      'country': 'US',
-      'limit': '1'
-    }).on('success', function (payload) {
-
-      $('.audioPlayer').eq(itemIndex).html(`<audio controls><source src='${payload.results[0].previewUrl}' type="audio/mpeg" </audio>`);
-    }).on('error', function (payload) {
-    alert("An error has occured. Please try again.");
-    });
-
-  }
-  )
-}
-function runScripts() {
-  $(watchHomeButton);
-  $(watchNavBar);
-  $(watchInput);
-};
-
-runScripts();
+$(function() {
+  watchInput();
+  watchNavBar();
+})
